cbb90a43175d6db7c32f386776ce28cf8c8c39fd
Vali <Valderamastork@yahoo.de>
2018-06-16 01:00:26 +0200

    wrongcommit test

--- src/moleMaxDefault/I2b2Import.java
+++ src/moleMaxDefault/I2b2Import.java
@@ -28,22 +28,10 @@ public class I2b2Import {
 	  	Statement statement = conn.createStatement();
 	  	statement.execute("DELETE FROM i2b2demodata.patient_mapping WHERE patient_ide_source='MoleMax'");
   		statement.execute("DELETE FROM i2b2demodata.patient_dimension WHERE patient_num>='11'");
-  		statement.execute("DELETE FROM i2b2demodata.observation_fact WHERE encounter_num='1337'");
-  		statement.execute("DELETE FROM i2b2demodata.visit_dimension WHERE encounter_num='1337'");
   		System.out.println("Alle Testdaten Gelöscht! ");
-    //	statement.execute("INSERT INTO i2b2demodata.visit_dimension(encounter_num, patient_num) VALUES ('1337', '11')");
-    //	statement.execute("INSERT INTO i2b2demodata.visit_dimension(encounter_num, patient_num) VALUES ('1337', '12')");
-    //	statement.execute("INSERT INTO i2b2demodata.visit_dimension(encounter_num, patient_num) VALUES ('1337', '13')");
-    //	statement.execute("INSERT INTO i2b2demodata.visit_dimension(encounter_num, patient_num) VALUES ('1337', '14')");
-    //	statement.execute("INSERT INTO i2b2demodata.visit_dimension(encounter_num, patient_num) VALUES ('1337', '15')");
 
   }
 
-  
-  
-  //Rinner Ffragen Lombok getter/stter?test1
-  
-  
 public void insertImageIntoI2b2(parseObjectImage image, Connection conn) {	
 //	+ "to_date('" + this.getBirthdate().toString() + "', 'YYYY-MM-DD')," +
 	Calendar rightNow = Calendar.getInstance();
@@ -53,36 +41,23 @@ public void insertImageIntoI2b2(parseObjectImage image, Connection conn) {
         Statement statement = conn.createStatement();
         String i2b2PatientId= null;
         //Nachschauen ob MoleMax ID des Patienten in der patient_mapping Tabelle des I2b2 vorhanden ist
-        ResultSet rsPatientMapping = statement.executeQuery("SELECT * FROM i2b2demodata.patient_mapping WHERE patient_ide_source='MoleMax' AND patient_ide='"+image.getPatientID()+"'");
-        if(rsPatientMapping.next()) {
+        ResultSet rs = statement.executeQuery("SELECT * FROM i2b2demodata.patient_mapping WHERE patient_ide_source='MoleMax' AND patient_ide='"+image.getPatientID()+"'");
+        if(rs.next()) {
         	//Falls vorhanden, id holen 
-        	i2b2PatientId = rsPatientMapping.getString("patient_num");
-         	ResultSet rsVisitDimension = statement.executeQuery("SELECT max(encounter_num) FROM i2b2demodata.visit_dimension WHERE patient_num='" + i2b2PatientId + "'");
-
+        	i2b2PatientId = rs.getString("patient_num");
             	 System.out.println("Patient bereits im System, i2b2Id: " + i2b2PatientId);
             	 
             	//Insert starten:
-            	 System.out.println("Inserte Observation_fact!!!");
-            	 //TODO: Nachsehen wann in die visit_dimension eingetragen wird
-            	int encounter_num_richtige = 1;
-            	int encounter_num_temp = 1;
-            	encounter_num_temp = encounter_num_richtige;
-             	int encounter_number=0;
-             	if(rsVisitDimension.next()) {
-             		encounter_number = rsVisitDimension.getInt(1);
-             	}
-             	System.out.println("Halloo:  " + encounter_number);
-             	if(encounter_number!=0) encounter_num_temp = encounter_number+1;
-             	
-            	statement.execute("INSERT INTO i2b2demodata.visit_dimension(encounter_num, patient_num) VALUES ('" + encounter_num_temp + "', '" + i2b2PatientId + "')");
-            	statement.execute("INSERT INTO i2b2demodata.observation_fact(encounter_num, patient_num, concept_cd, provider_id, start_date, modifier_cd, instance_num, valtype_cd, tval_char) VALUES ('1337','"+i2b2PatientId+"', 'skin_observation', 'N/A','2016-08-25 01:23:46.0','@', "+1+", '@', 'N/A')");  
+            	//statement.execute("INSERT INTO i2b2demodata.observation_fact(encounter_num, patient_num, concept_cd, provider_id, start_date, modifier_cd, instance_num, valtype_cd, tval_char) VALUES ('"+  +"', '"+i2b2PatientId+"'"     
             }else{
            //Falls nicht vorhanden, neue I2b2 Id erstellen und Alter und Sex in Patient Dimension eintragen
-            	rsPatientMapping=statement.executeQuery("SELECT max(patient_num) FROM i2b2demodata.patient_dimension");
-            	rsPatientMapping.next();
-            	int new_patient_num = rsPatientMapping.getInt(1)+1;
+            	rs=statement.executeQuery("SELECT max(patient_num) FROM i2b2demodata.patient_dimension");
+            	rs.next();
+            	int new_patient_num = rs.getInt(1)+1;
             	System.out.println("Patient noch nicht im System, neue Patient_num: " + new_patient_num);
-            //	int patientBirthYear = year - Integer.parseInt(image.getPatientAge());
+            	int patientBirthYear = year - Integer.parseInt(image.getPatientAge());
+            	System.out.println("Patienten Alter: " + image.getPatientAge());
+    			System.out.println("Patienten Geburtsdatum???:: " + patientBirthYear );
             	statement.execute("INSERT INTO i2b2demodata.patient_mapping(patient_ide, patient_ide_source, patient_num, project_id) VALUES ('"+image.getPatientID()+"' , 'MoleMax', '"+new_patient_num+"', 'MoleMax')");
             	statement.execute("INSERT INTO i2b2demodata.patient_dimension(patient_num, birth_date, age_in_years_num, sex_cd) VALUES ('"+ new_patient_num + "','2016-08-25 01:23:46.0','" +Integer.parseInt(image.getPatientAge()) +"','"+image.getPatientSex()+"')");
             	
@@ -90,6 +65,7 @@ public void insertImageIntoI2b2(parseObjectImage image, Connection conn) {
             	//statement.execute("INSERT INTO i2b2demodata.observation_fact(encounter_num, patient_num, concept_cd, provider_id, start_date, modifier_cd, instance_num, valtype_cd, tval_char) VALUES ('"+  +"', '"+i2b2PatientId+"'"     
             	//statement.execute("INSERT INTO i2b2demodata.observation_fact(encounter_num, patient_num, concept_cd, provider_id) VALUES ('"+ 1 +"', '"+new_patient_num+"','skin_observation', '"+  image.getDatasetName()+"')");    
 
+        	
         }    	
      // statement.execute("INSERT INTO i2b2demodata.patient_dimension(patient_num, age_in_years_num, sex_cd) VALUES ('"+ count +"', '"+Integer.parseInt(insert.getPatientAge()) +"', '"+insert.getPatientSex()+"')");
      // 	System.out.println("Image " + insert.getImageFilename() + " inserted");
